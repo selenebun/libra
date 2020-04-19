d8192d26909515dad62f9578cc38edba0b974c04
rsaihe <50564448+rsaihe@users.noreply.github.com>
2020-04-19 02:16:42 -0500

    Added Magic 8-Ball command.

--- src/commands/fun.rs
+++ src/commands/fun.rs
@@ -8,8 +8,42 @@ use serenity::prelude::*;
 use unicode_segmentation::UnicodeSegmentation;
 
 #[group]
-#[commands(shuffle)]
+#[commands(eight_ball, shuffle)]
 pub struct Fun;
+
+#[command("8ball")]
+#[description("Ask the Magic 8-Ball a question.")]
+#[min_args(1)]
+#[usage("question")]
+fn eight_ball(ctx: &mut Context, msg: &Message) -> CommandResult {
+    const ANSWERS: [&str; 20] = [
+        "It is certain.",
+        "It is decidedly so.",
+        "Without a doubt.",
+        "Yes\u{2014}definitely.",
+        "You may rely on it.",
+        "As I see it, yes.",
+        "Most likely.",
+        "Outlook good.",
+        "Yes.",
+        "Signs point to yes.",
+        "Reply hazy, try again.",
+        "Ask again later.",
+        "Better not tell you now.",
+        "Cannot predict now.",
+        "Concentrate and ask again.",
+        "Don't count on it.",
+        "My reply is no.",
+        "My sources say no.",
+        "Outlook not so good.",
+        "Very doubtful.",
+    ];
+
+    let answer = ANSWERS.choose(&mut SmallRng::from_entropy()).unwrap();
+    msg.channel_id.say(&ctx.http, answer)?;
+
+    Ok(())
+}
 
 #[command]
 #[aliases(scramble)]
@@ -17,8 +51,6 @@ pub struct Fun;
 #[min_args(1)]
 #[usage("word...")]
 fn shuffle(ctx: &mut Context, msg: &Message, args: Args) -> CommandResult {
-    let mut rng = SmallRng::from_entropy();
-
     // Break up each Unicode word into individual graphemes and shuffle.
     let words: String = args
         .message()
@@ -27,7 +59,7 @@ fn shuffle(ctx: &mut Context, msg: &Message, args: Args) -> CommandResult {
         .iter()
         .map(|word| {
             let mut graphemes = word.graphemes(true).collect::<Vec<&str>>();
-            graphemes.shuffle(&mut rng);
+            graphemes.shuffle(&mut SmallRng::from_entropy());
             graphemes.concat()
         })
         .collect();
